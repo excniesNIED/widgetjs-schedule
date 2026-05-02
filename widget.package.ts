a93880b45b242fcf2e7372818fcced3d1fe0d93b
excniesNIED <hxcn@cnies.org>
2026-05-02 16:44:19 +0800

    fix: 将项目信息中的 Schedule 翻译为日程管理（中英文）

--- widget.package.ts
+++ widget.package.ts
@@ -3,7 +3,8 @@ import { WidgetPackage } from '@widget-js/core'
 export default new WidgetPackage({
   author: 'excnies',
   description: {
-    'zh-CN': '支持课程表、待办与多格式导入导出的日程组件',
+    'zh-CN': '支持课程表、待办与多格式导入导出的日程管理组件',
+    'en-US': 'Schedule management widget with course schedules, todos, and multi-format import/export',
   },
   version: '1.0.0',
   entry: '/',
@@ -15,7 +16,8 @@ export default new WidgetPackage({
   homepage: '',
   name: 'cn.excnies.widget.schedule',
   title: {
-    'zh-CN': 'Schedule',
+    'zh-CN': '日程管理',
+    'en-US': 'Schedule',
   },
   devOptions: {
     folder: './src/widgets/',
