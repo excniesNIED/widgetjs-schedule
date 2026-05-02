0f144c2f163147903ade3d6b044dc02db53f403a
excniesNIED <hxcn@cnies.org>
2026-05-02 16:32:48 +0800

    feat: 增加版本、作者信息、仓库信息等配置

--- widget.package.ts
+++ widget.package.ts
@@ -5,12 +5,12 @@ export default new WidgetPackage({
   description: {
     'zh-CN': '支持课程表、待办与多格式导入导出的日程组件',
   },
+  version: '1.0.0',
   entry: '/',
   hash: true,
   remote: {
-    base: '/',
-    hostname: 'localhost',
-    hash: true,
+    base: '/widgetjs-schedule',
+    hostname: 'excniesnied.github.io',
   },
   homepage: '',
   name: 'cn.excnies.widget.schedule',
